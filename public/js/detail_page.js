5efc000567ad17f6bc9f7860387fd8cbc028382c
m0c0r30n <shotareon.0205@gmail.cm>
2021-02-09 19:30:22 +0900

    add detail

---
+++ public/js/detail_page.js
@@ -0,0 +1,15 @@
+$(function(){
+  $("#search_stock_number").keypress(function(e){
+    if(e.which == 13) {
+      if ($("#search_stock_number").val() !=  "") {
+        $(".search_stock .fa-search").click();
+      }
+    }
+  });
+
+  $(".search_stock .fa-search").on('click', function(){
+    var stock_number = $("#search_stock_number").val();
+    location.href = './stockdata/'+stock_number;
+  });
+
+});
--- resources/views/stockdatabase/detail.blade.php
+++ resources/views/stockdatabase/detail.blade.php
@@ -7,6 +7,11 @@
     <title>株データベース | 詳細ページ</title>
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/detail.css') }}">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="{{ asset('/js/detail_page.js') }}"></script>
+    <script src="https://kit.fontawesome.com/8f2f53141d.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   </head>
 <body>
   <header>
