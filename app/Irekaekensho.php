04470e46ac66891f213166da92d908477ff71452
m0c0r30n <shotareon.0205@gmail.cm>
2021-07-01 17:14:18 +0900

    create pdf

--- app/Irekaekensho.php
+++ app/Irekaekensho.php
@@ -8,6 +8,8 @@ use App\Irekaestock;
 
 class Irekaekensho extends Model
 {
+    protected $dates = ['date'];
+
     public function irekaestocks() {
         return $this->hasMany(Irekaestock::class,'irekaekensho_id')->orderBy('id','asc');
     }
