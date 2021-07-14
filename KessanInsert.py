0b7cf9c463a59f415d8768cd2f3a34037b85e848
m0c0r30n <shotareon.0205@gmail.cm>
2021-07-14 22:15:55 +0900

    fix kessanyotei

--- KessanInsert.py
+++ KessanInsert.py
@@ -9,15 +9,16 @@ import datetime
 import os
 import pandas as pd
 
-conn = MySQLdb.connect(
+ conn = MySQLdb.connect(
     host='localhost',
     port=3306,
     user='root',
-    password='',
-    database='stockdatabase',
+    password='moco0205',
+    database='kabudatabase',
     use_unicode=True,
     charset="utf8"
 )
+
 cur = conn.cursor()
 
 kessan_info_table = 'kessan_info'
