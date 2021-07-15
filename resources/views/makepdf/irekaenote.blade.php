74f3b09ed1c44b412c59d4931d2cc1d3b935e671
m0c0r30n <shotareon.0205@gmail.cm>
2021-07-15 22:19:40 +0900

    kessanyoteis

--- resources/views/makepdf/irekaenote.blade.php
+++ resources/views/makepdf/irekaenote.blade.php
@@ -3,13 +3,13 @@
 <head>
   <!-- <link rel="stylesheet" href="{{ asset('/css/app.css') }}"> -->
   <!-- <link rel="stylesheet" href="{{ asset('/css/review_pdf.css') }}">  -->
-    <title>PDF作成</title>
+    <title>抵抗線接近銘柄検証ノート</title>
   </head>
 <body>
   <?php $stock_count = count($irekae_stock); ?>
   @for ($n = 0; $n < $stock_count; $n++)
   <div class="page">
-    <h1 id="irekae_stock_title">{{ $irekae_kensho->date->format('m月d日') }}({{ $youbi }}) {{ $stock_name[$n] }}({{ $irekae_stock[$n]->stock_number }})</h2>
+    <h1 id="irekae_stock_title">{{$n}} {{ $irekae_kensho->date->format('m月d日') }} ({{ $youbi }}) {{ $stock_name[$n] }} ({{ $irekae_stock[$n]->stock_number }})</h2>
     <img src="https://stock-database.s3.ap-northeast-1.amazonaws.com/{{ $irekae_stock[$n]->irekae_before }}">
     <p class="stock_info">{!! nl2br(e($irekae_stock[$n]->info)) !!}</p>
   </div>
