f343fb9752950f37ef00668c61341f1c00b73e14
m0c0r30n <shotareon.0205@gmail.cm>
2021-07-21 18:55:55 +0900

    fix bug

--- resources/views/makepdf/irekaenote.blade.php
+++ resources/views/makepdf/irekaenote.blade.php
@@ -17,22 +17,22 @@
   <div class="page">
     <div class="indexes">
       <div class="nikkei_content">
+        <h2>日経平均株価</h2>
         <div class="nikkei_img">
           <img src="https://seacret-holder.s3.ap-northeast-1.amazonaws.com/nikkei/nikkei_{{ $irekae_kensho->hizuke->format('Y_m_d') }}.png">
         </div>
         <div class="nikkei_info">
-          <h2>日経平均株価</h2>
           <p>前日値 : {{ $nikkei_pastprice }}円</p>
           <p>始値 : {{ $nikkei_datas["openprice"] }}円</p>
           <p>終値 : {{ $nikkei_datas["endprice"] }}円</p>
         </div>
       </div>
       <div class="mothers_content">
+        <h2>マザーズ指数</h2>
         <div class="mothers_img">
           <img src="https://seacret-holder.s3.ap-northeast-1.amazonaws.com/mothers/mothers_{{ $irekae_kensho->hizuke->format('Y_m_d') }}.png">
         </div>
         <div class="mothers_info">
-          <h2>マザーズ指数</h2>
           <p>前日値 : {{ $mothers_pastprice }}円</p>
           <p>始値 : {{ $mothers_datas["openprice"] }}円</p>
           <p>終値 : {{ $mothers_datas["endprice"] }}円</p>
